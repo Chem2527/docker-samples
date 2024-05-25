b98d8a6ca52d5839ae7ea464eaf67e74fe95c3cd
Saikrishna <108404892+Chem2527@users.noreply.github.com>
2024-05-25 18:48:36 +0530

    Update redis.sh

--- healthchecks/redis.sh
+++ healthchecks/redis.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -eo pipefail
 
-host="$(hostname -i || echo '127.0.0.1')"
+# Use the service name from docker-compose.yml to determine the host IP
+host="redis"
 
+# Check if Redis server is reachable using redis-cli
 if ping="$(redis-cli -h "$host" ping)" && [ "$ping" = 'PONG' ]; then
-	exit 0
+    exit 0
 fi
 
 exit 1
